dd8c57ad746791082558f79594ad96fb2e178532
Ishaq Adulfatahi <48143761+abdulfatahi-ishaq@users.noreply.github.com>
2021-03-24 23:56:15 +0100

    Update Generalisimo-HNG-02735.js

--- scripts/Generalisimo-HNG-02735.js
+++ scripts/Generalisimo-HNG-02735.js
@@ -1,7 +1,4 @@
 var genId = "HNG-02735";
 var genName = "Emmanuel Ugwu";
 var genLang = "JavaScript";
-
-var fullId = "Hello World, this is" + " " + genName + " " + "with HNGi7 ID" + genId + " " + "using" + genLang + " " + "for stage 2 task."
-
-console.log(fullId);
+console.log("Hello World, this is" + " " + genName + " " + "with HNGi7 ID" + genId + " " + "using" + genLang + " " + "for stage 2 task.");
